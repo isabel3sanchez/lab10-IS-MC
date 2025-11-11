58bf1cd3056dde4ee012361301593cab22665102
Momo-PC <molinecharlesp@gmail.com>
2025-11-11 12:51:37 -0500

    merged changes

--- calculator.py
+++ calculator.py
@@ -25,18 +25,23 @@ import math
 
 def add(a, b):
     return a + b
+
 def sub(a, b):
     return a - b
+
 def mul(a, b):
     return a * b
+
 def div(a, b):
-    if a == 0:
-        raise ZeroDivisionError 
-    return b / b
+    if b == 0:
+        raise ZeroDivisionError("Can't divide by zero.")
+    return a / b
+
 def log(a, b):
-    if a <= 0 or a == 1 or b <=0:
-        raise ValueError
-    return math.log(b, a)
+    if a <= 0 or b <= 0 or b == 1:
+        raise ValueError("Log base must be positive and not equal to 1.")
+    return math.log(a, b)
+
 def exp(a, b):
     return a ** b
 
